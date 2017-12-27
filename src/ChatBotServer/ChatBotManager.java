b2a81055c162747b13702082b734ef01cbbc7c51
Alex Kir <alexvoorfontys@gmail.com>
2017-12-27 18:28:57 +0100

    get github user info

--- src/ChatBotServer/ChatBotManager.java
+++ src/ChatBotServer/ChatBotManager.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ChatBotManager extends UnicastRemoteObject implements IChatBotManager {
+    boolean githubLookUp = false;
     private List<Response> responses = new ArrayList<>();
     public ChatBotManager() throws RemoteException {
         responses.add(new Response("hallo","Hallo!"));
@@ -26,43 +27,54 @@ public class ChatBotManager extends UnicastRemoteObject implements IChatBotManag
     public String askQuestion(String question)  {
         if (question.toLowerCase().contains("bitcoin"))
         {
-            // build a URL
-            String s = "https://coinbin.org/btc";
-            URL url = null;
-            try {
-                url = new URL(s);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-
-            // read from the URL
-            Scanner scan = null;
-            try {
-                scan = new Scanner(url.openStream());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            String str = new String();
-            while (scan.hasNext())
-                str += scan.nextLine();
-            scan.close();
-
-            // build a JSON object
-            JSONObject obj = new JSONObject(str);
-            // get the first result
+            JSONObject obj = getJson("https://coinbin.org/btc");
             JSONObject res = obj.getJSONObject("coin");
-            return "$ " + String.valueOf(res.getInt("usd"));
+            return "De waarde van Bitcoin is $ " + String.valueOf(res.getInt("usd"));
         }
-        System.out.println(question.toLowerCase());
+        if (githubLookUp)
+        {
+            JSONObject obj = getJson("https://api.github.com/users/" + question);
+            githubLookUp = false;
+            return question + " : Naam: " + obj.getString("name")  + ", aantal openbare repositories: " + obj.getInt("public_repos")+ ", Volgers: " +
+                    obj.getInt("followers") + ", volgend: " + obj.getInt("following") +
+                    ", Link: " + obj.getString("html_url");
+        }
+        else if (question.toLowerCase().contains(" github"))
+        {
+            githubLookUp = true;
+            return "Verstuur een github gebruikersnaam in voor informatie erover";
+        }
         for (Response response:responses
              ) {
-            System.out.println(response.getQuestion().toLowerCase());
             if (question.toLowerCase().equals(response.getQuestion().toLowerCase()))
             {
-                System.out.println("test");
                 return response.getAnswer();
             }
         }
         return "ERROR: Geen reactie gevonden, voeg een reactie toe via de knop bovenin.";
+    }
+    private JSONObject getJson(String URL)
+    {
+        URL url = null;
+        try {
+            url = new URL(URL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        // read from the URL
+        Scanner scan = null;
+        try {
+            scan = new Scanner(url.openStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String str = new String();
+        while (scan.hasNext())
+            str += scan.nextLine();
+        scan.close();
+
+        // build a JSON object
+        return new JSONObject(str);
     }
 }
